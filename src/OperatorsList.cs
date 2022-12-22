c5d1b63832c322dc4f9f1496921d919ab36c2cc4
Baka632 <stevemc123456@outlook.com>
2022-12-22 12:48:39 +0800

    添加构造器

--- src/OperatorsList.cs
+++ src/OperatorsList.cs
@@ -1,17 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace ArknightsResources.Operators.Models
 {
     /// <summary>
     /// 干员列表
     /// </summary>
-    public sealed class OperatorsList
+    public sealed class OperatorsList : IEnumerable<Operator>
     {
         /// <summary>
         /// 包含<see cref="Operator"/>对象的字典
         /// </summary>
         private Dictionary<string, Operator> Operators { get; set; }
+
+        /// <summary>
+        /// 构造<see cref="OperatorsList"/>的新实例
+        /// </summary>
+        /// <param name="operators">包含<see cref="Operator"/>对象的字典</param>
+        [JsonConstructor]
+        public OperatorsList(Dictionary<string, Operator> operators)
+        {
+            Operators = operators;
+        }
+
+        /// <summary>
+        /// 构造<see cref="OperatorsList"/>的新实例
+        /// </summary>
+        /// <param name="operators">包含<see cref="Operator"/>对象的<see cref="IEnumerable{T}"/></param>
+        public OperatorsList(IEnumerable<Operator> operators)
+        {
+            int capacity = operators is ICollection<Operator> ops ? ops.Count : operators.Count();
+            Operators = new Dictionary<string, Operator>(capacity);
+            foreach (Operator item in operators)
+            {
+                Operators[item.Codename] = item;
+            }
+        }
 
         /// <summary>
         /// 通过干员代号获取指定的干员
@@ -49,6 +75,17 @@ namespace ArknightsResources.Operators.Models
                 index++;
             }
             return opArray;
+        }
+
+        /// <inheritdoc/>
+        public IEnumerator<Operator> GetEnumerator()
+        {
+            return Operators.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Operators.Values.GetEnumerator();
         }
     }
 }
